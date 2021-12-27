2f2b08d36cef6925b189724823814a73ed56f0b8
dhaydt <1992dayat@gmail.com>
2021-12-27 07:09:50 +0700

    remove twilio credentials

--- config/twilio.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'twilio_auth_sid' => env('TWILIO_AUTH_SID', 'AC65ed7495ca465aabd6f5fba47a5d4989'), // values : (local | production)
-    'twilio_auth_token' => env('TWILIO_AUTH_TOKEN', '535074ca082116fef3944231476fc851'), // values : (local | production)
-    'twilio_whatsapp_from' => env('TWILIO_WHATSAPP_FROM', '+14155238886'), // values : (local | production)
-];
